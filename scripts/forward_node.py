088685f8b93ca61e85209d8b12301ec07630142a
kmasaba21 <kmasaba21@gmail.com>
2020-07-28 01:13:11 -0400

    first commit from computer

--- scripts/forward_node.py
+++ scripts/forward_node.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Twist
+from std_msgs.msg import Bool
+from sensor_msgs.msg import JointState, BatteryState
+
 
 class Forward(object):
     def __init__(self):
         rospy.init_node('forward_node')
-        robot_name=rospy.get_param('~robot_name')
-        rospy.Subscriber('/pose',PoseStamped, self.pose_foward_callback)
-        self.pose_pub=rospy.Publisher('pose'.format(robot_name),PoseStamped,queue_size=1000)
+        robot_name = rospy.get_param('~robot_name')
+        rospy.Subscriber('/pose', PoseStamped, self.pose_forward_callback)
+        rospy.Subscriber('/cmd_vel', Twist, self.cmd_vel_forward_callback)
+        rospy.Subscriber('/reset_odom', Bool, self.reset_odom_forward_callback)
+        rospy.Subscriber('/joint_states', JointState, self.joint_state_forward_callback)
+        rospy.Subscriber('/battery', BatteryState, self.battery_forward_callback)
+        self.pose_pub = rospy.Publisher('pose'.format(robot_name), PoseStamped, queue_size=1000)
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel'.format(robot_name), Twist, queue_size=1000)
+        self.reset_odom_pub = rospy.Publisher('reset_odom'.format(robot_name), Twist, queue_size=1000)
+        self.jointstate_pub = rospy.Publisher('joint_states'.format(robot_name), JointState, queue_size=1000)
+        self.battery_pub = rospy.Publisher('battery'.format(robot_name), BatteryState, queue_size=1000)
 
-    def pose_foward_callback(self, data):
+    def pose_forward_callback(self, data):
         self.pose_pub.publish(data)
+
+    def cmd_vel_forward_callback(self, data):
+        self.cmd_vel_pub.publish(data)
+
+    def reset_odom_forward_callback(self, data):
+        self.reset_odom_pub.publish(data)
+
+    def joint_state_forward_callback(self, data):
+        self.jointstate_pub.publish(data)
+
+    def battery_forward_callback(self, data):
+        self.battery_pub.publish(data)
+
     def spin(self):
         rospy.spin()
 
-if __name__=='__main__':
-     Forward().spin()
+
+if __name__ == '__main__':
+    Forward().spin()
