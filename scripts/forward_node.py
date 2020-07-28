34fe95102df37da08772f4689cb08fdace5af6f4
John MacDonald <john.l.macdonald.22@dartmouth.edu>
2020-07-28 04:56:19 +0000

    remote changes

--- scripts/forward_node.py
+++ scripts/forward_node.py
@@ -1,15 +1,16 @@
+#!/usr/bin/python
 import rospy
 from geometry_msgs.msg import PoseStamped
 
-class Forward:
+class Forward(object):
     def __init__(self):
-        rospy.init('forward_node')
+        rospy.init_node('forward_node')
         robot_name=rospy.get_param('~robot_name')
         rospy.Subscriber('/pose',PoseStamped, self.pose_foward_callback)
-        self.pose_pub=rospy.Publisher('{}/pose'.format(robot_name),PoseStamped,queue_size=1000)
+        self.pose_pub=rospy.Publisher('pose'.format(robot_name),PoseStamped,queue_size=1000)
 
     def pose_foward_callback(self, data):
-        self.pose_pub.pub(data)
+        self.pose_pub.publish(data)
     def spin(self):
         rospy.spin()
 
